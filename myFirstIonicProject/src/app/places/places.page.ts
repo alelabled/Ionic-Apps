8accc64c15c89433f51890cbff557b225272a7cc
alelabled <alelabradorledesma@gmail.com>
2020-03-25 20:58:23 +0100

    #MFIA-0005

--- myFirstIonicProject/src/app/places/places.page.ts
+++ myFirstIonicProject/src/app/places/places.page.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { PlacesService } from "./places.service";
 
 @Component({
-  selector: 'app-places',
-  templateUrl: './places.page.html',
-  styleUrls: ['./places.page.scss'],
+  selector: "app-places",
+  templateUrl: "./places.page.html",
+  styleUrls: ["./places.page.scss"]
 })
 export class PlacesPage implements OnInit {
+  places = [];
 
-  constructor() { }
+  constructor(private placeService: PlacesService) {}
 
   ngOnInit() {
+    this.places = this.placeService.getPlaces();
   }
-
 }
